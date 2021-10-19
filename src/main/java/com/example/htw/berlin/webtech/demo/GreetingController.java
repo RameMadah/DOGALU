d60e64eba500a9a6b273928f8f3523e99b20f590
Rame Madah <lawkira10@gmail.com>
2021-10-19 03:04:11 +0200

    landing page

--- src/main/java/com/example/htw/berlin/webtech/demo/GreetingController.java
+++ src/main/java/com/example/htw/berlin/webtech/demo/GreetingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
         @GetMapping("/greeting")
         public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
             model.addAttribute("name", name);
-            return "greeting";
+            return "Index";
         }
     }
 
