83150961d65f6e2a8c73ef32ad38caee7c0e7108
Rame Madah <lawkira10@gmail.com>
2021-10-29 14:51:08 +0200

    connected DB

---
+++ src/main/java/com/example/htw/berlin/webtech/demo/UserRestController.java
@@ -0,0 +1,30 @@
+package com.example.htw.berlin.webtech.demo;
+
+
+import com.example.htw.berlin.webtech.demo.api.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserRestController {
+
+    private List<User> users;
+
+    public UserRestController(List<User> users) {
+        this.users = new ArrayList<>();
+        users.add(new User(1,"Rame","Madah","lawkira10@gmail.com"));
+    }
+
+    @GetMapping(path = "/api/v1/users")
+    public ResponseEntity<List<User>> fetchUsers (){
+    return ResponseEntity.ok(users);
+}
+
+
+
+
+}
