868e259685d8943a2fb21247c2ca8da73851a79d
Rame Madah <lawkira10@gmail.com>
2021-10-29 16:15:08 +0200

    JBDC_Variables

--- src/main/java/com/example/htw/berlin/webtech/demo/GreetingController.java
+++ src/main/java/com/example/htw/berlin/webtech/demo/GreetingController.java
@@ -1,12 +1,15 @@
 package com.example.htw.berlin.webtech.demo;
 
 
+import org.springframework.core.SpringProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-    @Controller
+import java.util.Optional;
+
+@Controller
     public class GreetingController {
 
         @GetMapping("/greeting")
@@ -17,9 +20,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
         @GetMapping("/")
-        public String greetin(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
             model.addAttribute("name", name);
             return "home";
         }
+
+        
     }
 
