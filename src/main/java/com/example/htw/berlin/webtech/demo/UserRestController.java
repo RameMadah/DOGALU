f8b4f43ffdfac548af4cb9dc95a4653d57c7e3e6
Rame Madah <lawkira10@gmail.com>
2021-11-02 18:17:13 +0100

    Mit Postman getestet

--- src/main/java/com/example/htw/berlin/webtech/demo/UserRestController.java
+++ src/main/java/com/example/htw/berlin/webtech/demo/UserRestController.java
@@ -2,8 +2,10 @@ package com.example.htw.berlin.webtech.demo;
 
 
 import com.example.htw.berlin.webtech.demo.web.api.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,16 +16,18 @@ public class UserRestController {
 
     private final List<User> users;
 
-    public UserRestController(List<User> users) {
+    public UserRestController() {
         this.users = new ArrayList<>();
         users.add(new User(1,"Rame","Madah","lawkira10@gmail.com"));
+        System.out.print("lllll");
         users.add(new User(2,"Franziska","Thier","franzua@gmail.com"));
 
     }
 
     @GetMapping(path = "/api/v1/users")
-    public ResponseEntity<List<User>> fetchUsers() {
-    return ResponseEntity.ok(users);
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public List<User> fetchUsers() {
+    return users ;
 }
 
 
